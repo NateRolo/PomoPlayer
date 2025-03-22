07856c8f1f202962980351adc0a084f4ede89c7f
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-03-21 23:12:24 -0700

    feat: implement Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -51,9 +52,8 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="dark">
       <body className={`${geistSans.variable} ${geistMono.variable} font-sans`}>
-        
-          {children}
-        
+        {children}
+        <Analytics />
       </body>
     </html>
   );
